72c3171b04321ec8b50248758e5848e562b4bd20
Xavi <xavi_tb@hotmail.com>
2020-12-29 21:22:49 +0100

    added terraform code

---
+++ terraform/eks/main.tf
@@ -0,0 +1,109 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "alexa-cluster"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "13.2.1"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.18"
+  subnets         = module.vpc.private_subnets
+  wait_for_cluster_cmd          = "until curl -k -s $ENDPOINT/healthz >/dev/null; do sleep 4; done"
+  wait_for_cluster_interpreter = ["C:/Program Files/Git/bin/sh.exe", "-c"]
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups = {
+    first = {
+      desired_capacity = 2
+      max_capacity     = 10
+      min_capacity     = 2
+
+      instance_type = "m5.large"
+    }
+  }
+
+  write_kubeconfig   = true
+  config_output_path = "./"
+
+  workers_additional_policies = [aws_iam_policy.worker_policy.arn]
+}
+
+resource "aws_iam_policy" "worker_policy" {
+  name        = "worker-policy"
+  description = "Worker policy for the ALB Ingress"
+
+  policy = file("iam-policy.json")
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "ingress" {
+  name       = "nginx-ingress-controller"
+  chart      = "nginx-ingress-controller"
+  repository = "https://charts.bitnami.com/bitnami"
+
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+}
+
+resource "helm_release" "alexa-skill" {
+  name       = "alexa-skill"
+  chart      = "../helm/alexa-skill-chart"
+  depends_on = [helm_release.ingress]
+}
